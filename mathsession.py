1d6a97eefa31b05860df35bc2f919fbdbf9dd7d6
srikargcv <122781766+srikargcv@users.noreply.github.com>
2025-05-26 20:03:25 +0530

    May 26th Session

---
+++ mathsession.py
@@ -0,0 +1,60 @@
+
+# Functions
+
+# a + b = c
+
+# a + b -> LHS
+# c -> RHS
+
+# sum(a,b) = c
+
+# f(x) = y
+
+# f - function Name
+# x - variable
+# y - result / Constant
+
+# prod(a,b) = c
+
+# Prod -> a * b = c
+
+# f(x) = y
+
+# Sequence of Objects - Baseline Functions - rely on multiple objects
+
+# 1 , 2 , 3 , 4
+
+# 2 , 4 , 6 , 8
+
+
+a = 10
+
+# Collection - List, Set, Tuple, Dictionary
+
+print(a)
+
+a = 20
+a = 30
+a = 40
+
+print(a)
+
+# List - []
+# Unlimited Range - No limit to the Number of elemetns inside the List
+# Store elements / multiple items in single variable
+
+# List items are ordered, Changed, Allow Duplicates
+
+# Every element in the list is having a Index value
+
+# 0. Chips
+# 1. Paste
+# 2. XYZ
+
+# Numbering to the List of elements in Ascending order from 0 to n-1 : Indexing
+
+# First element Index : 0
+
+# Last Element index : n-1
+
+# Where n is Total number of elements in a List
